576faec25310af83def3b0501b3c60a7b6cd85dd
Himanshu Pandey <himanshu.dn.pandey@gmail.com>
2023-02-19 07:16:22 +0530

    Add Middleware

--- backend-app/main.py
+++ backend-app/main.py
@@ -4,9 +4,23 @@ from sqlalchemy.orm import Session
 import crud, models, schemas
 from database import SessionLocal, engine
 
+from fastapi.middleware.cors import CORSMiddleware
+
 models.Base.metadata.create_all(bind=engine)
 
 app=FastAPI()
+
+origins=['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#Middlewares
+
 
 # Dependency
 def get_db():
